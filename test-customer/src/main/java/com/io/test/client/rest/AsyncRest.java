f6edd36d9173e009d6298f9b4a07fe19999b6902
zhu-wt <1490020533@qq.com>
2021-12-27 09:24:10 +0800

    style:添加注释

--- test-customer/src/main/java/com/io/test/client/rest/AsyncRest.java
+++ test-customer/src/main/java/com/io/test/client/rest/AsyncRest.java
@@ -6,11 +6,11 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 异步调用
+ *
  * @Author: wangtianzhu
  * @Date: 2021-12-17
  */
@@ -24,13 +24,13 @@ public class AsyncRest {
     @ApiOperation("异步调用task")
     @PostMapping("/task")
     public void task() throws InterruptedException {
-        for (int i=0; i < 3;  i++){
+        for (int i = 0; i < 3; i++) {
             long t1 = System.currentTimeMillis();
             asyncTaskService.doTask1();
             asyncTaskService.doTask2();
             Thread.sleep(1000);
             long t2 = System.currentTimeMillis();
-            log.info("main cost {} ms", t2-t1);
+            log.info("main cost {} ms", t2 - t1);
         }
     }
 }
